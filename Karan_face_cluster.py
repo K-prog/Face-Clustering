1326c5480b24e0f96ce62c8b0fe09dc3079c5bbe
K-prog <52241581+K-prog@users.noreply.github.com>
2023-01-26 02:57:04 +0530

    Update Karan_face_cluster.py

--- Karan_face_cluster.py
+++ Karan_face_cluster.py
@@ -3,11 +3,6 @@ import numpy as np
 import cv2
 import os
 from skimage.io import imread_collection
-import argparse
-import logging
-
-#set_logging("Face cluster by Karan Singh")  # run before defining LOGGER
-LOGGER = logging.getLogger("Face cluster by Karan Singh")
 
 pose_predictor = dlib.shape_predictor('shape_predictor_5_face_landmarks.dat')       # importing dlib models
 face_encoder = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
